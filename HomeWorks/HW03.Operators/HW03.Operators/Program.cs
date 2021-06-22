7153b7ceca250fcd46afb8e3517dbbecc2cd2a5e
Khodzin Ilya <Khodzin@gmail.com>
2021-06-22 20:23:00 +0300

    Task 1

---
+++ HomeWorks/HW03.Operators/HW03.Operators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW03.Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumTwoNumbers();
+        }
+
+        //Запрос 2 переменных и вывод их суммы
+        static void SumTwoNumbers()
+        {
+            double number1, number2;
+            Console.WriteLine("Введите первое число:");
+            number1 = GetNumberWithCheck();
+            Console.WriteLine("Введите второе число:");
+            number2 = GetNumberWithCheck();
+            Console.WriteLine($"{number1}+{number2}={number1+number2}");
+        }
+
+        //Получение числа с проверкой на правильность ввода
+        static double GetNumberWithCheck()
+        {
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out double number))
+                    return number;
+                else Console.WriteLine("Введенный символ не является числом, повторите попытку");
+            }            
+        }
+
+    }
+}
