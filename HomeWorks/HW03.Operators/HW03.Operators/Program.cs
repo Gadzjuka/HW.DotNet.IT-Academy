a663f9736f7182651a124ebf28a7396938c1cce6
Khodzin Ilya <Khodzin@gmail.com>
2021-06-22 21:16:08 +0300

    Task 4 (+ and -)

--- HomeWorks/HW03.Operators/HW03.Operators/Program.cs
+++ HomeWorks/HW03.Operators/HW03.Operators/Program.cs
@@ -9,25 +9,43 @@ namespace HW03.Operators
             SumTwoNumbers();
         }
 
-        //Запрос 2 переменных и вывод их суммы (Задача 1)
+        //Запрос 2 переменных и вывод их суммы (Задача 4)
         static void SumTwoNumbers()
         {
             double number1, number2, answer;
+            char mathSimbol;
             Console.WriteLine("Введите первое число:");
             number1 = GetNumberWithCheck();
             Console.WriteLine("Введите второе число:");
             number2 = GetNumberWithCheck();
-            Console.WriteLine("Введите сумму чисел:");
-            answer = GetNumberWithCheck();
-            Console.WriteLine($"{number1}+{number2}={number1+number2}");
+            Console.WriteLine("Введите символ \'+\' или \'-\':");
+            mathSimbol = GetMathSimbolWithCheck();
 
-
-            if (answer == (number1 + number2)) Console.WriteLine("Правильно");
-            else
+            if (mathSimbol == '+')      //Если сумма (+)
             {
-                if (answer > (number1 + number2)) Console.WriteLine("Неправильно. Должно быть меньше.");
-                else Console.WriteLine("Неправильно. Должно быть больше.");
-            }            
+                Console.WriteLine("Введите сумму чисел:");
+                answer = GetNumberWithCheck();
+                Console.WriteLine($"{number1}+{number2}={number1 + number2}");
+                if (answer == (number1 + number2)) Console.WriteLine("Правильно");
+                else
+                {
+                    if (answer > (number1 + number2)) Console.WriteLine("Неправильно. Должно быть меньше.");
+                    else Console.WriteLine("Неправильно. Должно быть больше.");
+                }
+
+            }
+            else                        //Если разница (-)
+            {
+                Console.WriteLine("Введите разницу чисел:");
+                answer = GetNumberWithCheck();
+                Console.WriteLine($"{number1}-{number2}={number1 - number2}");
+                if (answer == (number1 - number2)) Console.WriteLine("Правильно");
+                else
+                {
+                    if (answer > (number1 - number2)) Console.WriteLine("Неправильно. Должно быть меньше.");
+                    else Console.WriteLine("Неправильно. Должно быть больше.");
+                }
+            }     
         }
 
         //Получение числа с проверкой на правильность ввода
@@ -41,5 +59,18 @@ namespace HW03.Operators
             }            
         }
 
+        //Получение + или - с проверкой на правильность ввода
+        static char GetMathSimbolWithCheck()
+        {
+            while (true)
+            {
+                if (char.TryParse(Console.ReadLine(), out char simbol))
+                {
+                    if (simbol == '+' || simbol == '-') return simbol;
+                    else Console.WriteLine("Введенный символ не допустим");
+                }
+                else Console.WriteLine("Сочетание символов не допустимо");
+            }
+        }
     }
 }
