89c97e847396791a77dd03469d4f9f9005db90a5
Khodzin Ilya <Khodzin@gmail.com>
2021-06-23 01:11:21 +0300

    Done!

---
+++ HomeWorks/ParseAndTryParse/ParseAndTryParse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParseAndTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "112233";
+            int x = int.Parse(number);
+            int.TryParse(number, out int y);
+        }
+    }
+}
