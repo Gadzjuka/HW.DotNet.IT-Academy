8b2e05dd00aeb9370f7b056b1a49ecaf47c5b343
Khodzin Ilya <Khodzin@gmail.com>
2021-06-22 21:22:48 +0300

    Task 5 - reverse alphabet with FOR

---
+++ HomeWorks/HW03.Operators/HW03.ReverseAlphabet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW03.ReverseAlphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i >= 65; i--)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+    }
+}
